f4b09cb8f03f7e2e4bc27ed5c9c7b9e12fa21b97
human <human@mychromebookhaha>
2024-12-27 21:40:52 -0800

    migrated to glut

--- main.c
+++ main.c
@@ -1,8 +1,5 @@
-#include <X11/Xlib.h>
-#include <X11/XKBlib.h>
-#include <X11/Xutil.h>
 #include <GL/gl.h>
-#include <GL/glx.h>
+#include <GL/glut.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,10 +12,7 @@
 #define WHEIGHT 600
 #define WTITLE "EPIC BREAKOUT :3"
 
-static Display* display;
-static Window window;
-static XVisualInfo* visual;
-static GLXContext gl_context;
+int window;
 
 static int quit;
 
@@ -47,7 +41,7 @@ static float PADDLE_SPEED = 0.015f;
 static float paddle_x;
 static float paddle_y;
 
-static KeySym unreleased_key;
+static int unreleased_key;
 
 static int idling;
 
@@ -143,171 +137,156 @@ void next_level(){
   PADDLE_SPEED *= LVLUP_MUL;
 }
 
+void input(unsigned char ascii, int x, int y){
+  switch(ascii){
+  case 0x1b:
+    quit = 1;
+    break;
+  case ' ':
+    if(idling){
+      idling = 0;
+      ball_vx = LAUNCH_VX;
+      ball_vy = LAUNCH_VY;
+    }
+    break;
+  }
+}
+
+void special_input(int key, int x, int y){
+  unreleased_key = key;
+}
+
+void special_input_up(int key, int x, int y){
+  unreleased_key = 0;
+}
+
+void loop();
+
 void init(){
-  display = XOpenDisplay(NULL);
-  window = XCreateSimpleWindow(display,
-				      XDefaultRootWindow(display),
-				      0, 0,
-				      WWIDTH, WHEIGHT,
-				      0,
-				      0x00000000,
-				      0x00000000);
+  int argc = 0;
+  char* argv = "";
+  glutInit(&argc, &argv);
+  glutInitWindowSize(WWIDTH, WHEIGHT);
+  glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
   
-  XStoreName(display, window, WTITLE);
-  XSelectInput(display, window, KeyPressMask | KeyReleaseMask | StructureNotifyMask);
-  XMapWindow(display, window);
-
-  unreleased_key = XK_VoidSymbol;
-  XAutoRepeatOff(display);
+  window = glutCreateWindow(WTITLE);
+  glutShowWindow();
   
-  visual = glXChooseVisual(display, 0, (int[]){
-      GLX_RGBA,
-      GLX_DEPTH_SIZE, 24,
-      GLX_DOUBLEBUFFER,
-      None
-    });
-  printf("Visual ID %lx\n", visual->visualid);
+  unreleased_key = 0;
+  glutSetKeyRepeat(GLUT_KEY_REPEAT_OFF);
   
-  gl_context = glXCreateContext(display, visual, 0, True);
-  glXMakeCurrent(display, window, gl_context);
-
   glViewport(-1, -1, WWIDTH, WHEIGHT);
 
   spreadBricks();
   resetPaddle();
   newBall();
+  
+  quit = 0;
+  
+  glutKeyboardFunc(input);
+  glutSpecialFunc(special_input);
+  glutSpecialUpFunc(special_input_up);
+  glutDisplayFunc(loop);
+  glutMainLoop();
 }
 
 void cleanup(){
-  glXDestroyContext(display, gl_context);
-  XAutoRepeatOn(display);
-  XCloseDisplay(display);
+  glutSetKeyRepeat(GLUT_KEY_REPEAT_ON);
+  glutDestroyWindow(window);
+  exit(0);
 }
 
 void loop(){
-  quit = 0;
-  KeySym ks;
-  while(!quit){
-    /* == INPUT == */
-    
-    while(XPending(display) > 0){
-      XEvent event = {0};
-      XNextEvent(display, &event);
-      switch(event.type){
-      case KeyPress:
-	ks = XkbKeycodeToKeysym(display, event.xkey.keycode, 0, 0);
-	switch(ks){
-	case XK_Escape:
-	  quit = 1;
-	  break;
-	case XK_space:
-	  if(idling){
-	    idling = 0;
-	    ball_vx = LAUNCH_VX;
-	    ball_vy = LAUNCH_VY;
-	  }
-	  break;
-	default:
-	  unreleased_key = ks;
-	  break;
-	}
-	break;
-      case KeyRelease:
-	ks = XkbKeycodeToKeysym(display, event.xkey.keycode, 0, 0);
-	if(ks == unreleased_key)
-	  unreleased_key = XK_VoidSymbol;
-	break;
-      }
-    }
-    
-    switch(unreleased_key){
-    case XK_Left:
-      paddle_x -= PADDLE_SPEED;
-      LAUNCH_VX = -fabsf(LAUNCH_VX);
-      break;
-    case XK_Right:
-      paddle_x += PADDLE_SPEED;
-      LAUNCH_VX = fabsf(LAUNCH_VX);
-      break;
-    }
-    
-    /* == PROCESSING (PHYSICS, GENERAL) == */
+  if(quit) cleanup();
 
-    if(paddle_x + PADDLE_WIDTH/2.0f > 1.0f) paddle_x -= PADDLE_SPEED;
-    else if(paddle_x - PADDLE_WIDTH/2.0f < -1.0f) paddle_x += PADDLE_SPEED;
-
-    if(idling) ball_x = paddle_x;
-    ball_x += ball_vx;
-    ball_y += ball_vy;
-
-    if(ball_x + BALL_R > 1.0f || ball_x - BALL_R < -1.0f){
-      ball_vx *= -1.0f;
-      ball_x += ball_vx;
-    }
-    if(ball_y + BALL_R > 1.0f ||
-       (ball_x - BALL_R <= paddle_x + PADDLE_WIDTH/2.0f &&
-	ball_x + BALL_R >= paddle_x - PADDLE_WIDTH/2.0f &&
-	ball_y - BALL_R <= paddle_y + PADDLE_HEIGHT/2.0f)){
-      ball_vy *= -1.0f;
-      ball_y += ball_vy;
-    }
-    if(ball_y - BALL_R < -1.0f){
-      newBall();
-    }
-
-    if(ball_y + BALL_R >= bricks_y[0] - BR_HEIGHT){
-      int brid;
-      float prevd = 69.0f;
-      float newd;
-      
-      for(int i=0; i<BR_ROWS*BR_COLS; ++i){
-	newd = VEC_D(ball_x, ball_y, bricks_x[i], bricks_y[i]);
-	if(newd <= prevd){
-	  brid = i;
-	  prevd = newd;
-	}
-      }
-
-      if(bricks_active[brid]){
-	float brx = bricks_x[brid];
-	float bry = bricks_y[brid];
-	if(ball_x - BALL_R <= brx + BR_WIDTH &&
-	   ball_x + BALL_R >= brx - BR_WIDTH &&
-	   ball_y - BALL_R <= bry + BR_HEIGHT &&
-	   ball_y + BALL_R >= bry - BR_HEIGHT)
-	  {
-	    float nx, ny;
-	    nx = ball_x - brx;
-	    ny = ball_y - bry;
-	    nx /= fabs(nx);
-	    ny /= fabs(ny);
-	    ball_vx *= -nx;
-	    ball_vy *= ny;
-
-	    bricks_active[brid] = 0;
-	    --bricks_active_count;
-	    if(bricks_active_count <= 0) next_level();
-	}
-      }
-    }
-    
-    /* == RENDERING == */
-    
-    glClearColor(BGCOL, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    drawBorder();
-    drawPaddle();
-    drawBall();
-    drawBricks();
-
-    glXSwapBuffers(display, window);
+  /* == INPUT == */
+  switch(unreleased_key){
+  case GLUT_KEY_LEFT:
+    paddle_x -= PADDLE_SPEED;
+    LAUNCH_VX = -fabsf(LAUNCH_VX);
+    break;
+  case GLUT_KEY_RIGHT:
+    paddle_x += PADDLE_SPEED;
+    LAUNCH_VX = fabsf(LAUNCH_VX);
+    break;  
   }
+
+  
+  /* == PROCESSING (PHYSICS, GENERAL) == */
+  if(paddle_x + PADDLE_WIDTH/2.0f > 1.0f) paddle_x -= PADDLE_SPEED;
+  else if(paddle_x - PADDLE_WIDTH/2.0f < -1.0f) paddle_x += PADDLE_SPEED;
+  
+  if(idling) ball_x = paddle_x;
+  ball_x += ball_vx;
+  ball_y += ball_vy;
+  
+  if(ball_x + BALL_R > 1.0f || ball_x - BALL_R < -1.0f){
+    ball_vx *= -1.0f;
+    ball_x += ball_vx;
+  }
+  if(ball_y + BALL_R > 1.0f ||
+     (ball_x - BALL_R <= paddle_x + PADDLE_WIDTH/2.0f &&
+      ball_x + BALL_R >= paddle_x - PADDLE_WIDTH/2.0f &&
+      ball_y - BALL_R <= paddle_y + PADDLE_HEIGHT/2.0f)){
+    ball_vy *= -1.0f;
+    ball_y += ball_vy;
+  }
+  if(ball_y - BALL_R < -1.0f){
+    newBall();
+  }
+  
+  if(ball_y + BALL_R >= bricks_y[0] - BR_HEIGHT){
+    int brid;
+    float prevd = 69.0f;
+    float newd;
+    
+    for(int i=0; i<BR_ROWS*BR_COLS; ++i){
+      newd = VEC_D(ball_x, ball_y, bricks_x[i], bricks_y[i]);
+      if(newd <= prevd){
+	brid = i;
+	prevd = newd;
+      }
+    }
+    
+    if(bricks_active[brid]){
+      float brx = bricks_x[brid];
+      float bry = bricks_y[brid];
+      if(ball_x - BALL_R <= brx + BR_WIDTH &&
+	 ball_x + BALL_R >= brx - BR_WIDTH &&
+	 ball_y - BALL_R <= bry + BR_HEIGHT &&
+	 ball_y + BALL_R >= bry - BR_HEIGHT)
+	{
+	  float nx, ny;
+	  nx = ball_x - brx;
+	  ny = ball_y - bry;
+	  nx /= fabs(nx);
+	  ny /= fabs(ny);
+	  ball_vx *= -nx;
+	  ball_vy *= ny;
+	  
+	  bricks_active[brid] = 0;
+	  --bricks_active_count;
+	  if(bricks_active_count <= 0) next_level();
+	}
+    }
+  }
+  
+  /* == RENDERING == */
+  
+  glClearColor(BGCOL, 1.0);
+  glClear(GL_COLOR_BUFFER_BIT);
+  
+  drawBorder();
+  drawPaddle();
+  drawBall();
+  drawBricks();
+
+  glutSwapBuffers();
+  glutPostRedisplay();
 }
 
 int main(void){
   init();
-  loop();
   cleanup();
   
   return 0;
